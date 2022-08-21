4484703e8adc409cd767d15b9de13b3fa7e062ba
Huey-J <wkdgus7113@gmail.com>
2022-08-21 15:57:30 +0900

    first commit

---
+++ src/main/java/com/fmar/travelrhythm/TravelrhythmApplication.java
@@ -0,0 +1,13 @@
+package com.fmar.travelrhythm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelrhythmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelrhythmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmar/travelrhythm/TravelrhythmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmar.travelrhythm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelrhythmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
